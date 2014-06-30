8209611f92037ba57557b5215f865acf7ab5fc76
Sundeep Sivaraman <sundeep.sivaraman@gmail.com>
2014-06-29 20:21:30 -0700

    Added the next() in-order iterator method

--- Arena/src/com/hipsandy/ds/BinSearchTree.java
+++ Arena/src/com/hipsandy/ds/BinSearchTree.java
@@ -8,10 +8,15 @@ public class BinSearchTree<T extends Comparable<T>> {
 		Node<T> right;
 		Node<T> left;
 		
+		Node<T> parent;
+		
 		public Node(T val, Node<T> left, Node<T> right) {
 			this.val = val;
 			this.left = left;
 			this.right = right;
+			
+			this.left.parent = (left == null) ? null : this;
+			this.right.parent = (right == null) ? null : this;
 		}
 		
 		public Node(T val) {
@@ -34,6 +39,7 @@ public class BinSearchTree<T extends Comparable<T>> {
 
 		public void setRight(Node<T> right) {
 			this.right = right;
+			this.right.parent = (right == null) ? null : this;
 		}
 
 		public Node<T> getLeft() {
@@ -42,6 +48,7 @@ public class BinSearchTree<T extends Comparable<T>> {
 
 		public void setLeft(Node<T> left) {
 			this.left = left;
+			this.left.parent = (left == null) ? null : this;
 		}
 	}
 	
@@ -66,6 +73,27 @@ public class BinSearchTree<T extends Comparable<T>> {
 	
 	public Node<T> delete(T val) {
 		return delete(root, val);
+	}
+	
+	public Node<T> next(Node<T> n) {
+		if (n == null) {
+			throw new IllegalArgumentException("Invalid entry");
+		}
+		if (n.right != null) {
+			return findMin(n.right);
+		} else {
+//			Node<T> currNode = n;
+//			while(n.parent != null && n.parent.val.compareTo(currNode.val) <= 0) {
+//				n = n.parent;
+//			}
+//			return n.parent;
+			
+			Node<T> p = n.parent;
+			while(p != null && p.val.compareTo(n.val) <= 0) {
+				p = p.parent;
+			}
+			return p;
+		}
 	}
 	
 	private Node<T> delete(Node<T> n, T val) {
